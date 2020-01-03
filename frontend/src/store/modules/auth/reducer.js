85416387390e7d212b8b372166808e1a791dec2f
RicardoGorki <ricardo.gorki@gmail.com>
2020-01-03 12:04:52 -0300

    fix redux user

--- frontend/src/store/modules/auth/reducer.js
+++ frontend/src/store/modules/auth/reducer.js
@@ -19,11 +19,15 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.loading = false;
         break;
       }
-      case '@auth/SIGN_IN_FAILURE': {
+      case '@auth/SIGN_FAILURE': {
         draft.loading = false;
         break;
       }
-
+      case '@auth/SIGN_OUT': {
+        draft.token = null;
+        draft.signed = false;
+        break;
+      }
       default:
     }
   });

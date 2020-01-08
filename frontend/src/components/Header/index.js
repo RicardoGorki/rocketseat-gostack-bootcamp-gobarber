b28f3d6ee69726d2cdc9648059ed446728292a75
RicardoGorki <ricardo.gorki@gmail.com>
2020-01-08 17:38:15 -0300

    fix: avatar url

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -7,6 +7,8 @@ import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const logoTemp = 'https://api.adorable.io/avatars/50/abott@adorable.png';
+
   const profile = useSelector(state => state.user.profile);
 
   return (
@@ -24,10 +26,7 @@ export default function Header() {
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/abott@adorable.png'
-              }
+              src={profile.avatar ? profile.avatar.url : logoTemp}
               alt="Ricardo Gorki"
             />
           </Profile>
